266cd42dfedf5c135ea4f49cfe494e145e7230d1
Sujay Shandilaya <shandilayasujay@gmail.com>
2021-12-19 00:09:09 -0600

    Removed access keys

--- Twitter_Data_Insights/twitter_data_extractor.py
+++ Twitter_Data_Insights/twitter_data_extractor.py
@@ -2,15 +2,18 @@ from tweepy.streaming import Stream
 from tweepy import OAuthHandler
 from tweepy import Stream
 import json
-#import boto3
+import boto3
 #import time
 
 
 #Variables that contains the user credentials to access Twitter API
-consumer_key = 'NwfLCUHI8i0jgEqECukZQCRVj'
-consumer_secret ='yfPMRSDzSBGGhanrpjmcDDzyIG2eR8KzPK8Hxx2P9f1xuglqpB'
-access_token = '1472086647860867074-uwjUnOYF5SQnhLfIrYwGFgMJxj9dqG'
-access_token_secret = 'U9kJo0acfKE0YAe8RulmFgvwlBDUC0EsCdaqF3EVCCu3m'
+consumer_key = '#############'
+consumer_secret ='###############'
+access_token = '###################'
+access_token_secret = '#########################'
+
+access_key_id = '##################'
+secret_access_key = '##################'
 
 
 class StdOutListener(Stream):
@@ -29,30 +32,32 @@ class StdOutListener(Stream):
         
         
         else:
-            message_dict= { 'id' : str(tweet['id']),
+            message_dict={ 'id' : str(tweet['id']),
                         'name' : str(tweet['user']['name']),
                         'text' : tweet['text'],
                         'followers_count' : str(tweet['user']['followers_count']),
                         'location' : str(tweet['user']['location']),
                         'hashtags' : tweet['entities']['hashtags']
-                        }            
+                        }
+
+        message_dict = json.dumps(message_dict, indent=2).encode('utf-8')
                
    
-        f = open("myfile2.txt", "a")
-        f.write(json.dumps(message_dict, indent=4))
-        f.write('\n')
-        f.close()
-        
-        f = open("myfile.txt", "a")
-        f.write(json.dumps(tweet, indent=4))
-        f.write('\n')
-        f.close()
+        #f = open("myfile2.txt", "a")
+        #f.write(json.dumps(message_dict, indent=4))
+        #f.write('\n')
+        #f.close()
+        #
+        #f = open("myfile.txt", "a")
+        #f.write(json.dumps(tweet, indent=4))
+        #f.write('\n')
+        #f.close()
         #print(msg.encode("utf-8") for msg in message_lst)
         
         client.put_record(
                     DeliveryStreamName=delivery_stream,
                     Record={
-                    'Data': message
+                    'Data': message_dict
                     }
                 )
         
@@ -67,6 +72,13 @@ if __name__ == '__main__':
     #auth = OAuthHandler(consumer_key, consumer_secret)
     #auth.set_access_token(access_token, access_token_secret)
     
+    client = boto3.client('firehose', 
+                          region_name='us-east-1',
+                          aws_access_key_id=access_key_id,
+                          aws_secret_access_key=secret_access_key
+                          )
+
+    delivery_stream = 'PUT-S3-Twitter-Data'
     
     while True:
         try:
